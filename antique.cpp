adc1ae679a1cdc6f019fa40746d5dd86cb0df6f7
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-03-18 23:31:19 -0500

    Overload the == operator and + operator for Antique class.

--- antique.cpp
+++ antique.cpp
@@ -40,3 +40,28 @@ Antique::Antique()
 	price = 0;
 }
 
+bool Antique::operator==(const Antique& other)
+{
+	bool isSame = false;
+
+	//Using compare() to compare two strings
+	if ((price == other.price) && (name.compare(other.name) == 0))
+	{
+		isSame = true;
+	}
+
+	return isSame;
+}
+
+Antique Antique::operator+(const Antique& other)
+{
+	Antique antiqueTotal;
+
+	antiqueTotal.price = price + other.price;
+
+	//Creating a string stream
+	ostringstream outSS;
+
+	outSS << name << " + " << other.name;
+	antiqueTotal.name = outSS.str();
+}
